05d4fbab2626253fc7ad322f81bebad6609bdeab
chrisfishback <35583832+chrisfishback@users.noreply.github.com>
2018-12-02 14:41:46 -0800

    Update State.java

--- State.java
+++ State.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 /**
  * state class
  *
@@ -9,26 +12,29 @@
 public class State
 {
     private boolean acceptState;
+    public List<Integer> zeroList;
+    public List<Integer> oneList;
     private int zero;
     private int one;
     
     /**
      * Constructor for objects of class state
      */
-    public State(int z, int o, int accept)
+    public State(List<Integer> z, List<Integer> o, int accept)
     {
         if(accept == 0)
             acceptState = true;
         else
             acceptState = false;
         
-        zero = z;
-        one = o;
+        zeroList = z;
+        oneList = o;
     }
 
     public boolean checkAccept()
     {
         return acceptState;
     }
+    
     
 }
