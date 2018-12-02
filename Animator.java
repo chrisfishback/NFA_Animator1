3b6f4c21ed079db5f2619188f606369b4273ae5e
jacobsa20 <35584304+jacobsa20@users.noreply.github.com>
2018-12-02 12:37:37 -0800

    Update Animator.java

--- Animator.java
+++ Animator.java
@@ -6,6 +6,9 @@
  * 
  * @version 1.0
  */
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
 public class Animator
 {
     private NFA nfa;
@@ -14,10 +17,19 @@ public class Animator
     /**
      * Constructor for objects of class Animator
      */
-    public Animator()
+    protected int x; // my x coordinate
+    protected int y; // my y coordinate
+    protected int diameter = 50; //my diameter
+    public Animator(int initX, int initY)
     {
         nfa = new NFA();
         currentState = -1;
+        JFrame myFrame = new JFrame();
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setSize(1000,1000);
+        this.x = initX;
+        this.y = initY;
+        this.diameter = 20;
     }
 
     public void animateNFA()
@@ -25,6 +37,11 @@ public class Animator
         
     }
     
+    public void paint (Graphics canvas)
+    {
+        canvas.setColor(Color.WHITE);
+        canvas.fillOval(x, y, diameter, diameter);
+    }
     public void printInputString()
     {
         
