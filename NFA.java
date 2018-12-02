4269a5589c3603dd9d66851e9f1a50d47914d3ec
chrisfishback <35583832+chrisfishback@users.noreply.github.com>
2018-12-02 12:11:12 -0800

    Update NFA.java
    first push

--- NFA.java
+++ NFA.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.io.*;
 
 /**
  * NFA class
@@ -12,18 +13,71 @@ public class NFA
 {
     private ArrayList<State> listOfStates;
     private boolean isNFA;
+    int startState;
     
     /**
      * Constructor for objects of class NFA
      */
     public NFA()
     {
-        listOfStates = new ArrayList<State>(4);
+        listOfStates = new ArrayList<State>();
         isNFA = false;
     }
 
-    public void setStates()
+    public void createNFA()
     {
+        //for seeing the file
+        String fileName = "testCase1.txt";
+        String line = null;
         
+        //big poo
+        //info for states
+        int lineNum = 1; //for line in text file
+        int zeroTrans = 0;
+        int oneTrans = 0;
+        int accState = -1;
+        
+        try {
+            // FileReader reads text files in the default encoding
+            FileReader fileReader = new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+                
+                if(lineNum == 1)
+                    startState = Integer.parseInt(line);
+                
+                if(lineNum > 1)
+                {
+                    String[] tmp = line.split(" ");    //Split at the spaces
+                    
+                    zeroTrans = Integer.parseInt(tmp[1]);
+                    oneTrans = Integer.parseInt(tmp[2]);
+                    accState = Integer.parseInt(tmp[3]);
+                    
+                    listOfStates.add(new State(zeroTrans, oneTrans, accState));
+                
+                }
+                
+                System.out.println(zeroTrans);
+                System.out.println(oneTrans);
+                System.out.println(accState);
+                
+                System.out.println(listOfStates);
+                System.out.println("\n\n\n\n\n");
+                
+                lineNum++;
+            }
+
+            //close file
+            bufferedReader.close();
+        } 
+        catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
     }
+
 }
